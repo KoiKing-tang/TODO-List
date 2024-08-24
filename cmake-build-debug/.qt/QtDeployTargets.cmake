7273e90f5df4c03dc720d2daa06000ce5d89b237
KoiKing <Lain.o.iot@gamil.com>
2024-08-24 23:01:45 +0800

    Initial commit

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ToDoList_FILE C:/Users/Lain/CLionProjects/ToDoList/cmake-build-debug/ToDoList.exe)
+set(__QT_DEPLOY_TARGET_ToDoList_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ToDoList_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
